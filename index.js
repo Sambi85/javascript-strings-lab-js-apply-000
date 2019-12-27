9f9169c863d26540f36fbfcf5155dea4044fda8b
Samuel Lesser <ineedmoreplates@gmail.com>
2019-12-27 02:09:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + {specialGuest} + "!";
 
 var topic = "space";
 
 var conversation = "Let's talk about " +`${topic}.`;
-https://learn.co/tracks/technical-application-javascript/introduction/welcome/strings-lab#
