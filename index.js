935a6e553fd31a042f40d424e58b3c77ba3e9b24
Samuel Lesser <ineedmoreplates@gmail.com>
2019-12-27 02:06:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
 var conversation = "Let's talk about " +`${topic}.`;
+https://learn.co/tracks/technical-application-javascript/introduction/welcome/strings-lab#
